d1332c6aa89301a7f921ea84e4dfa327ecfe381f
Muhammad Abdul Ghani Qureshi <itsabdulghaniqureshi@gmail.com>
2023-08-13 21:58:06 -0700

    Node splitting is remaining

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const fs = require("fs");
 
+const uuid = require("uuid");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -27,6 +29,7 @@ app.get("/recommend", (req, res) => {
   res.render("recommend");
 });
 const filePath = path.join(__dirname, "data", "restaurants.json");
+
 app.get("/restaurants", (req, res) => {
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -36,9 +39,24 @@ app.get("/restaurants", (req, res) => {
   });
 });
 
+app.get("/restaurants/:id", (req, res) => {
+  const restaurantId = req.params.id;
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", {
+        restaurant: restaurant,
+      });
+    }
+  }
+  res.render("404");
+});
+
 app.post("/recommend", (req, res) => {
   const restaurant = req.body;
-
+  restaurant.id = uuid.v4();
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
 
@@ -47,5 +65,12 @@ app.post("/recommend", (req, res) => {
   fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 
   res.redirect("/confirm");
+});
+
+app.use((req, res) => {
+  res.render("404");
+});
+app.use((error, req, res, next) => {
+  res.render("500");
 });
 app.listen(3000);
