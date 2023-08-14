0651bde35dedc6c2a3b28e758e34a50749992aa4
Muhammad Abdul Ghani Qureshi <itsabdulghaniqureshi@gmail.com>
2023-08-13 22:59:13 -0700

    Project Finished

--- app.js
+++ app.js
@@ -1,76 +1,27 @@
-const express = require("express");
+const path = require('path');
 
-const path = require("path");
+const express = require('express');
 
-const fs = require("fs");
-
-const uuid = require("uuid");
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/confirm", (req, res) => {
-  res.render("confirm");
-});
-app.get("/recommend", (req, res) => {
-  res.render("recommend");
-});
-const filePath = path.join(__dirname, "data", "restaurants.json");
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
-app.get("/restaurants", (req, res) => {
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
+app.use(function (req, res) {
+  res.status(404).render('404');
 });
 
-app.get("/restaurants/:id", (req, res) => {
-  const restaurantId = req.params.id;
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render("restaurant-detail", {
-        restaurant: restaurant,
-      });
-    }
-  }
-  res.render("404");
+app.use(function (error, req, res, next) {
+  res.status(500).render('500');
 });
 
-app.post("/recommend", (req, res) => {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-  res.redirect("/confirm");
-});
-
-app.use((req, res) => {
-  res.render("404");
-});
-app.use((error, req, res, next) => {
-  res.render("500");
-});
 app.listen(3000);
